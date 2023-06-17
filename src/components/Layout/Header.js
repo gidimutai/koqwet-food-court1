8df71076eea85dd4c2fe8e5dfb7c68764c8d91de
gidimutai <101206849+gidimutai@users.noreply.github.com>
2023-06-17 14:05:30 +0300

    semis

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -9,7 +9,7 @@ import {
   Divider
  } from '@mui/material'
 import MenuIcon from '@mui/icons-material/Menu';
-import { Link } from 'react-router-dom';
+import { NavLink, Link } from 'react-router-dom';
 import '../../styles/HeaderStyles.css'
 
 
@@ -82,16 +82,16 @@ const Header = () => {
           <Box sx={{display: {xs: 'none', sm: 'block'}}}>
              <ul className='navigation-menu'>
               <li>
-                <Link to={"/"}>Home</Link>
+                <NavLink activeClassName='active' to={"/"}>Home</NavLink>
               </li>
               <li>
-                <Link to={"/menu"}>Menu</Link>
+                <NavLink to={"/menu"}>Menu</NavLink>
               </li>
               <li>
-                <Link to={"/about"}>About</Link>
+                <NavLink to={"/about"}>About</NavLink>
               </li>
               <li>
-                <Link to={"/contact"}>Contact</Link>
+                <NavLink to={"/contact"}>Contact</NavLink>
               </li>
             </ul> 
           </Box>
--- src/data/data.js
+++ src/data/data.js
@@ -1,40 +1,198 @@
-import fo3 from '../images/food/3.jpg'
 import fo4 from '../images/food/4.jpg'
 import fo5 from '../images/food/5.jpg'
 import fo6 from '../images/food/6.jpg'
-import fo7 from '../images/food/7.jpg'
+import beefkaranga from '../images/food/beefkaranga.jpg'
+import beetrootjuice from '../images/food/beetrootcarotjuice.jpg'
+import bhajia from '../images/food/bhajia.jpg'
+import chaimasala from '../images/food/chaimasala.jpg'
+import chapati from '../images/food/chapati.jpg'
+import chips from '../images/food/chipsmayai.webp'
+import coconut from '../images/food/coconutcurndamonmilkshake.webp'
+import friedfish from '../images/food/friedfish.webp'
+import nyamchom from '../images/food/nyamchom.webp'
+import samosa from '../images/food/samosas.webp'
+import mursik from '../images/food/mursik.jpg'
+import pawpawjuice from '../images/food/pawpawjuice.webp'
+import mutura from '../images/food/mutura.webp'
+import mrenda from '../images/food/mrenda.webp'
+import mandazi from '../images/food/mandazi.webp'
+import milk from '../images/food/milk.jpg'
+import mshkaki from '../images/food/mshkaki.webp'
+import matoke from '../images/food/matokebeef.webp'
+import mashedpotatoes from '../images/food/mushedpotatos.webp'
+import kukupaka from '../images/food/kukupaka.webp'
+import kachumbari from '../images/food/kachumbari.webp'
+import pilau from '../images/food/pilau.webp'
+import coleshawsalad from '../images/food/coleshawsalad.webp'
+
+
 
 export const MenuList = [
+  
   {
-    name:'fo3',
-    description:'blablabla',
-    image: fo3,
+    name:'SAMOSA',
+    description:' It consists of a crispy and flaky pastry shell filled with a savory mixture of spiced potatoes, peas, and sometimes meat or other vegetables',
+    image: samosa,
+    price: 200,
+  }, 
+  {
+    name:'MURSIK',
+    description:'It is a thick and tangy beverage with a distinctive smoky flavor. Mursik is prepared by fermenting milk in a specially prepared gourd known as a "sotet" or "biriket," which imparts a unique taste to the milk',
+    image: mursik,
+    price: 200,
+  },
+  
+  {
+    name:'PAWPAW JUICE',
+    description:'Enjoy the goodness of our Pawpaw Juice, packed with nutrients and bursting with flavor. Indulge in a healthy and refreshing experience with every sip!',
+    image: pawpawjuice,
     price: 200,
   },
   {
-    name:'fo4',
-    description:'blablabla',
-    image: fo4,
+    name:'MUTURA',
+    description:' food that consists of a spicy sausage made from a mixture of minced meat, offal, and a variety of spices. It isgrilled or roasted to perfection, resulting in a flavorful and juicy snack',
+    image: mutura,
+    price: 200,
+  },
+  {
+    name:'MRENDA',
+    description:"Savor the goodness of mrenda, a nutritious and flavorsome leafy green vegetable that brings a taste of Kenya's culinary heritage to your plate. Enjoy its unique flavors and reap the health benefits of this versatile ingredient!",
+    image: mrenda,
+    price: 200,
+  },
+  {
+    name:'MANDAZI',
+    description:' It is a delightful snack that is enjoyed throughout Kenya. Mandazi has a soft and fluffy texture, with a slightly sweet and aromatic flavor.Satisfy your cravings and transport yourself to the vibrant culinary scene of Kenya!',
+    image: mandazi,
+    price: 200,
+  },
+  {
+    name:'UGALI, KIENYEJI, MILK',
+    description:'Experience the traditional flavors of Kenya with Ugali with kienyeji and milk, adds a creamy and nutritious element to the meal!',
+    image: milk,
+    price: 200,
+  },
+  {
+    name:'MSHKAKI',
+    description:' its deliciously marinated and grilled skewered meat. It is a flavorful and aromatic dish that is commonly enjoyed by many as it is  made with small chunks of tender meat, such as beef or goat, which are marinated in a mixture of spices and then grilled to perfection. The result is a mouthwatering and savory treat that is perfect for meat lovers.',
+    image: mshkaki,
+    price: 200,
+  },
+  {
+    name:'MATOKE BEEF',
+    description:'Matoke beef is a traditional East African dish that combines tender beef with matoke, a type of green banana or plantain. It is a flavorful and hearty dish that is popular and healthy. ',
+    image: matoke,
+    price: 200,
+  },
+  {
+    name:'MASHED POTATOES',
+    description:'Indulge in the comforting combination of creamy mashed potatoes and sweet peas. This classic side dish brings together the smooth texture of mashed potatoes with the delightful burst of flavor from the peas. Enjoy it as a versatile accompaniment to enhance your mea',
+    image: mashedpotatoes,
+    price: 200,
+  },
+  
+  
+  {
+    name:'KUKUPAKA(CHIXKEN CURRY)',
+    description:'Kuku Paka is a flavorful and creamy coconut chicken curry. It is a rich and aromatic dish made with chicken pieces cooked in a coconut milk-based sauce, infused with a blend of spices and herbs. ',
+    image: kukupaka,
+    price: 200,
+  },
+  
+  
+  {
+    name:'KACHUMBARI',
+    description:'Enjoy the refreshing and tangy flavors of kachumbari, a versatile East African salad that brings together the natural sweetness of tomatoes, the crispness of onions and cucumbers, and the freshness of herbs. Indulge in this vibrant side dish that adds a burst of flavor to your meal and complements a wide range of dishes',
+    image: kachumbari,
+    price: 200,
+  },
+  
+  
+  {
+    name:'PILAU',
+    description:'This deliceous food is a one-pot dish that combines rice, meat (such as chicken, beef, or lamb), or vegetables with a blend of spices and aromatic ingredients. Pilau is known for its rich and savory taste, often enhanced with ingredients like onions, garlic, ginger, and a variety of spices',
+    image: pilau,
+    price: 200,
+  },
+  
+
+  {
+    name:'BEETROOT JUICE',
+    description:'Enjoy the goodness of our Beetfruit Juice, packed with nutrients and bursting with flavor. Indulge in a healthy and refreshing experience with every sip!',
+    image: beetrootjuice,
+    price: 200,
+  },
+  {
+    name:'BHAJIA',
+    description:"Made by slicing boiled potatoes into thin, circular slices, dipping them into a seasoned gram flour batter, and then deep-frying them. Served hot with ketchup sauces and/or kachumbari. They're very crispy and very filling. They taste incredible",
+    image: bhajia,
+    price: 200,
+  },
+  {
+    name:'CHAIMASALI',
+    description:' Chai means tea in Swahili. Kenyan tea is famous all over the world, and when combined with other ingredients like ginger or cardamom, the flavor is delicious beyond imagination.,',
+    image: chaimasala,
+    price: 200,
+  },
+  {
+    name:'Chips Mayai',
+    description:'This is a combination of French fries and an egg omelet. It is mostly served with kachumbari and tomato sauce',
+    image: chips,
+    price: 200,
+  },
+  {
+    name:'CHAPATI',
+    description:'These are soft and layered flatbreads. It is very light and is eaten with tea, beef stew, or bean',
+    image: chapati,
+    price: 200,
+  },
+  {
+    name:'BEEF WET FRY',
+    description:'Beef Wet Fry is a delicious and flavorful dish made with tender pieces of beef that are marinated and cooked in a rich and aromatic sauce. It is a popular dish in various cuisines, particularly in East African and Indian cuisine',
+    image: beefkaranga,
+    price: 200,
+  },
+  {
+    name:'COCONUTMILKSHAKE',
+    description:'Savor the tropical goodness of our Coconut Milkshake, a creamy and delightful treat that will transport you to a sunny paradise with every sip!',
+    image: coconut,
+    price: 200,
+  },
+  {
+    name:'Tilapia fry',
+    description:'Tilapia is widely eaten fish and very popular in Kenya because it is readily available and affordable. It is deep-fried and served with kachumbari, ugali, or pilau',
+    image: friedfish,
+    price: 200,
+  },
+  {
+    name:'MUKIMO',
+    description:'Mukimo is a tasty staple food cooked by mashing green peas, potatoes, maize, and pumpkin leaves or spinach leaves altogether',
+    image:fo4,
     price: 300,
   },
   {
-    name:'fo5',
-    description:'blablabla',
+    name:'UGALI',
+    description:'Experience the authentic taste of East Africa with ugali, a comforting and versatile staple that complements a wide array of flavorful dishes!',
     image: fo5,
     price: 400,
   },
   {
-    name:'fo6',
-    description:'blablabla',
+    name:'FISH WETFRY',
+    description:'Savor the tantalizing flavors of Kenyan-style Fish Wet Fry, a delightful lakefood dish that highlights the vibrant tastes of satistying cuisine!',
     image: fo6,
     price: 100,
   },
   {
-    name:'fo7',
-    description:'blablabla',
-    image: fo7,
+    name:'COLESHAW SALAD',
+    description: "This is a popular salad in Kenyan cuisine, and it goes with almost every main dish. It goes very well with grilled chicken or grilled fish. It's a salad that's made up of cabbage, carrots, onion, celery, apple or pineapple, and mayonnaise",
+    image: coleshawsalad,
+    price: 200,
+  }, 
+  {
+    name:'NYAMA CHOMA',
+    description:' Nyama Choma is a popular East African dish that consists of grilled or roasted meat,goat or beef, marinated in a flavorful blend of spices. Enjoy with a side dish called Kachumbari, which is a fresh and tangy tomato and onion salad. This combination creates a delicious and satisfying meal .',
+    image: nyamchom,
     price: 100,
-  }
-
+  },
 
 ]
